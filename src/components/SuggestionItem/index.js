eef29b8cea0f695c006af24782001193eebdadc6
chandu310 <chandunaidu310.com>
2023-03-24 06:48:29 +0000

    suggestions google

---
+++ src/components/SuggestionItem/index.js
@@ -0,0 +1,23 @@
+import './index.css'
+
+const SuggestionItem = props => {
+  const {suggestionDetails, updateSearchInput} = props
+  const {suggestion} = suggestionDetails
+
+  const onClickSuggestion = () => {
+    updateSearchInput(suggestion)
+  }
+
+  return (
+    <li className="suggestion-item">
+      <p className="suggestion-text">{suggestion}</p>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/diagonal-arrow-left-up.png"
+        alt="arrow"
+        className="arrow"
+        onClick={onClickSuggestion}
+      />
+    </li>
+  )
+}
+export default SuggestionItem
